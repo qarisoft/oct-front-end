a91aa6dbb6744696509019bf2339dd554dcf1c65
salah <cloud.data.sa@gmail.com>
2025-02-12 03:15:31 +0300

    pnpm

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { Chart1 } from "@/components/index/chart1"
 import { Chart2 } from "@/components/index/chart2"
-import { Chart3 } from "@/components/index/chart3"
 import { Chart4 } from "@/components/index/chart4"
 import { DataTable } from "@/components/index/data-table"
 import {
@@ -46,16 +45,10 @@ export default function Page() {
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3 ">
 
-            {/* <div className="aspect-video rounded-xl bg-muted/50" /> */}
             <Chart1/>
             <Chart2/>
-            {/* <Chart3/> */}
             <Chart4/>
-
-            {/* <div className="aspect-video rounded-xl bg-muted/50" /> */}
-            {/* <div className="aspect-video rounded-xl bg-muted/50" /> */}
           </div>
-          {/* <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min bg-slate-950" /> */}
           <div className="flex justify-center">
             <div className=" flex-1 px-5">
               <DataTable/>
